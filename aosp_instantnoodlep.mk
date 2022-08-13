e7052510272c961baf8ff6ef6baebb59f0ece2eb
dark-free <emchaouki@gmail.com>
2022-08-13 15:03:07 +0000

    Instantnoodlep: PixelPlusUi bring-up
    Signed-off-by: dark-free <emchaouki@gmail.com>

--- aosp_instantnoodlep.mk
+++ aosp_instantnoodlep.mk
@@ -27,6 +27,16 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1440
 
+#PPUI Stuff
+IS_PHONE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_instantnoodlep
 PRODUCT_DEVICE := instantnoodlep
